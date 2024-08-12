a223bcffca3a1e193cd7d03bc78fcffce7263673
unknown <[alswp936@naver.com]>
2024-08-12 17:57:46 +0900

    0812

---
+++ two_towers_0812.py
@@ -0,0 +1,4 @@
+T = int(input())
+for tc in range(1, T+1):
+    N, M1, M2 = map(int, input().split())
+    #  greedy 활용
--- wherearewords.py
+++ wherearewords.py
@@ -2,17 +2,17 @@ def shipza(N, K):
     cnt = 0
     for i in range(N):
         for j in range(N):
-            h = 0
-            v = 0
-            for k in range(K):
+            h = []  # 가로식
+            v = []  # 세로식
+            for k in range(K):  # 글자수에 대해서
                 if arr[i][j] == 1 and 0 <= i + k < N and 0 <= j + k < N:
-                    #가로일 때
-                    h += arr[i][j+k]
-                    #세로일 때
-                    v += arr[i+k][j]
-            if h == K:
+                    # 가로일 때
+                    h.append([i][N-j-1])  # 한 열 다 돌았을 때
+                    # 세로일 때
+                    v.append([N-i-1][j])  # 글자 길이까지
+            if h == K:  # h가 글자 길이와 같으면
                 cnt += 1
-            elif v == K:
+            elif v == K:  # v가 글자 길이와 같으면
                 cnt += 1
     return cnt
 
